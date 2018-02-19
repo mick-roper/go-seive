0ce714b2c49df283b5b4766bebd55fc1c6ce6bde
Mick Roper <michael.roper@drax.com>
2018-02-19 11:46:25 +0000

    seive is in and working

--- app/main.go
+++ app/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"sort"
+	"time"
+)
+
+var keys []int
+var seive map[int]bool
 
 func main() {
-	log.Print("Hello, World!")
+	nPtr := flag.Int("n", 100, "the upper ceiling")
+
+	flag.Parse()
+
+	n := *nPtr
+
+	seive = make(map[int]bool, n)
+
+	log.Println("Creating seive...")
+
+	for i := 2; i <= n; i++ {
+		seive[i] = true
+	}
+
+	for i := range seive {
+		keys = append(keys, i)
+	}
+
+	log.Println("sorting keys...")
+
+	sort.Ints(keys)
+
+	log.Println("Seiving for primes...")
+
+	start := time.Now()
+
+	for p := 2; p <= n; {
+		for i := p * p; i <= n; i += p {
+			seive[i] = false
+		}
+
+		if p*p > n { // break out of the loop here
+			break
+		}
+
+		for i := range keys {
+			if i > p && seive[i] {
+				p = i
+				break
+			}
+		}
+	}
+
+	elapsed := time.Since(start)
+
+	log.Print("Seive complete!")
+
+	for i := range keys {
+		if seive[i] {
+			log.Println(i)
+		}
+	}
+
+	log.Printf("took %v milliseconds to calculate %v primes", elapsed.Nanoseconds()/1000000, n)
 }
